deea1852d1101fc781a099b83f90edd3667fe852
alrantisi <ilhammubarokalrantisi@mail.ugm.ac.id>
2024-09-02 17:08:46 +0700

    Create Praktikum 4 Week 2.py

---
+++ Week 2/Praktikum 4 Week 2.py
@@ -0,0 +1,8 @@
+print ("Praktikum Pemrograman Komputer")
+print ("Berikut Contoh Program Penambahan :")
+a = 2
+b = 3
+hasil = a + b
+print ("hasil = a + b")
+print ("hasil = %d + %d" % (a,b))
+print ("hasil = %d" % (hasil))
