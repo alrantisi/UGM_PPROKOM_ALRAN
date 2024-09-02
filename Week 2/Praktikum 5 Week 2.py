abbed46d72f9984cfdf590751a32ed290256f0cf
alrantisi <ilhammubarokalrantisi@mail.ugm.ac.id>
2024-09-02 17:10:19 +0700

    Create Praktikum 5 Week 2.py

---
+++ Week 2/Praktikum 5 Week 2.py
@@ -0,0 +1,14 @@
+# Mencari Kesalahan / Debugging
+print ("Hello, World!")
+print (2 + 2),
+Print ("Test 123")
+a = 2 * 4
+print (aa)
+
+#menjadi
+
+print("Hello, World!")
+print(2 + 2)
+print("Test 123")
+a = 2 * 4
+print(a)
