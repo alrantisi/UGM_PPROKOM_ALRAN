c8a866241c6d356a2fdf2a75ea088a7ee5870a35
Rantesheesh <ilhammubarokalrantisi@mail.ugm.ac.id>
2024-09-18 19:16:22 +0700

    Create Praktikum 1 Week 6.py

---
+++ Week 6/Praktikum 1 Week 6.py
@@ -0,0 +1,10 @@
+#Program Menambah Elemen Baru
+
+#Deklarasi variabel berisi tipe data set
+nilai = set({3,6,9,2,5,7}) #Baris 4-5
+print(nilai)
+
+#Tambahkan nilai lebih dari satu digit menggunakan fungsi update
+nilai.update({1,2,3,4,5,6,7,8})#Baris 8-10
+nilai.update([9,10])
+print(nilai)
